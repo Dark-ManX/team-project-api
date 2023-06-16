756416a8e8f9752e975d6203c1c0c9e25e6a043b
Serhii <sergiosis@ukr.net>
2023-06-16 09:17:14 +0300

    in process

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
-// 15gyhIvZRzillrzY
+
 const logger = require("morgan");
 const cors = require("cors");
 
--- server.js
+++ server.js
@@ -4,18 +4,20 @@ require("colors");
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const { PORT = 2222 } = process.env.PORT;
-const DB_HOST = process.env.MONGO_URL;
+// const { PORT = 2222 } = process.env.PORT;
+const PASSWORD = "15gyhIvZRzillrzY";
+const DB_HOST = `mongodb+srv://Serhii:${PASSWORD}@my-cluster.koqzhr5.mongodb.net/Database?retryWrites=true&w=majority`;
+// process.env.MONGO_URL;
 
-const connection = mongoose.connect(DB_HOST, {
-  promiseLibrary: global.Promise,
-  useUnifiedTopology: true,
-});
+const connection = mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err.message));
 
 connection
   .then(() => {
-    app.listen(PORT, function () {
-      console.log(`Server running on port: ${PORT}`.bold.cyan.italic);
+    app.listen(2222, function () {
+      console.log(`Server running on port: 2222`.bold.cyan.italic);
     });
   })
   .catch((err) => {
